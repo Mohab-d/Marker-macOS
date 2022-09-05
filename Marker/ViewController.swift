bffc31e9f674f2fcffc448bad58b08c39af38d7b
Mohammad Mohab <mohabuisness.dev@gmail.com>
2022-09-05 09:31:54 +0200

    Initial Commit

---
+++ Marker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Marker
+//
+//  Created by IMac on 05/09/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
